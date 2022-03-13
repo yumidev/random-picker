4fb2d248824456da113a550cd9c1111270d6a559
Yumi Ko <yumi.ko.cheers@gmail.com>
2022-03-13 19:03:15 +0900

    add pseudo code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # random-picker
 Provide randomly picked result from inputted data
+
+```
+./random
+```
---
+++ random-picker.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string input = "";
+  cin >> input;
+  // parse input into a vector(array has to predefine its size, so it's not proper to use in this case because we don't know the size of options yet)
+  // * array, vector, list
+  // get the size of vector,
+  // draw a number from 0 to the size of vector, 
+  // use that number as an index and choose value from vector
+  // print or return the selected value
+  cout << input;
+  return 0;
+}
