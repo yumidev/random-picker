e36d03234e069b5a91f562e51014d286c987ff94
Yumi Ko <yumi.ko.cheers@gmail.com>
2022-03-16 00:14:12 +0900

    add refs

--- random-picker.cc
+++ random-picker.cc
@@ -12,6 +12,8 @@ int main() {
 
   // parse input into a vector(array has to predefine its size, so it's not proper to use in this case because we don't know the size of options yet)
   // * array, vector, list
+  // ref: https://www.techiedelight.com/split-a-string-into-a-vector-in-cpp/
+  // or ref: https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
   
   // get the size of vector,
   
