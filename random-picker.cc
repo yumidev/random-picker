1f00b4c483f3e5c65fce0529d64fc7728209a140
Yumi Ko <yumi.ko.cheers@gmail.com>
2022-03-15 23:53:27 +0900

    change file extension

--- random-picker.cc
+++ random-picker.cc
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <vector>
+// #include <boost>
 using namespace std;
 
 int main() {
   string input = "";
+  vector<string> arr;
+  int selectedIndex = 0;
   cin >> input;
+  cout << input;
+
   // parse input into a vector(array has to predefine its size, so it's not proper to use in this case because we don't know the size of options yet)
   // * array, vector, list
+  
   // get the size of vector,
+  
   // draw a number from 0 to the size of vector, 
+  
   // use that number as an index and choose value from vector
+  selectedIndex = 1; // assign randomly picked number here 
   // print or return the selected value
-  cout << input;
+  // cout << arr[selectedIndex];
+
   return 0;
 }
